1be5a49c58fc4ccda6dd836c04c7d3de6197c83d
MehdiHarrak <mehdi.elharrak.mehdi@gmail.com>
2025-02-27 13:55:56 +0000

    Tp3 initial commit

---
+++ TP3_GesrtionRH/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP3_GesrtionRH
+{
+    public abstract class  Person
+    {
+        private int code;
+        private string nom;
+        private string prenom;
+
+        public int Code
+        {
+            get { return code; }
+            set { code = value; }
+        }
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+        public string Prenom
+        {
+            get { return prenom; }
+            set { prenom = value; }
+        }
+    }
+    
+}
