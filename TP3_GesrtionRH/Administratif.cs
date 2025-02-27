fd841051bbf4c04f44be441b639b17d9c5c242a0
Adam El Matni <elmatniadam5@gmail.com>
2025-02-27 14:31:37 +0000

    administratif et Groupe classes

---
+++ TP3_GesrtionRH/Administratif.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP3_GesrtionRH
+{
+    public class Administratif : Personel
+    {
+        public Administratif(int bureau, double salaire, double prime, string code, string nom, string prenom) : base(bureau, salaire, prime, code, nom, prenom)
+        {
+        }
+    }
+}
---
+++ TP3_GesrtionRH/Groupe.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP3_GesrtionRH
+{
+    internal class Groupe
+    {
+        private string nom;
+        private List<Etudiant> etudiants;
+
+        public Groupe(string nom){
+            this.nom=nom;
+            this.etudiants = new List<Etudiant>();
+        }
+
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+
+        public List<Etudiant> Etudiants
+        {
+            get { return etudiants; }
+        }
+
+    }
+}
